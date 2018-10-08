437c61bf7acc7474927aa729155ec8d735e47423
tprax <tprax01@gmail.com>
2018-10-08 10:24:07 -0600

    add controller and routes

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -4,4 +4,18 @@ class Coin < ApplicationRecord
 
   has_many :watched_coins, dependent: :destroy
   has_many :users, through: :watched_coins
+
+  def self.create_by_cmc_id(res)
+    match = res['data'].with_indifferent_access
+
+    coin_params = {
+      name: matched[:name],
+      symbol: matched[:symbol],
+      cmc_id: matched[:id]
+    }
+
+    Coin.find_or_create_by(coin_params) do |coin|
+      coin.price = match[:quotes][:USD][:price]
+      coin.last_fetched = DateTime.now
+  end
 end
